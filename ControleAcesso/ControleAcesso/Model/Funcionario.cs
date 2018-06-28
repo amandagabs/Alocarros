0756e471c78ccc7639653d905e9d7e9ce499a524
Paulo Sérgio <Paulo Srgio@DESKTOP-SL328QM>
2018-06-28 10:30:47 -0300

    Criação de botões pagina principal Cadastro.

---
+++ ControleAcesso/ControleAcesso/Model/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleAcesso.Model
+{
+    class Funcionario
+    {
+        public int FuncionarioID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+
+        public string Nasc { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Empresa { get; set; }
+
+        public string Horario { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
